917567b5ee84717eecabda1a5a7fe928d6140181
Alexander Moore <alexandermoore202@gmail.com>
2025-08-04 20:47:27 -0400

    Got rid of tailwind config. Just using globals.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="bg-primary w-full h-[950px]">
-      <h2>Colleen McMillan Tutoring</h2>;
+      <h2 className="font-sans">Colleen McMillan Tutoring</h2>;
     </div>
   );
 }
