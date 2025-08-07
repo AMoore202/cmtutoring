34e1a49dc437996afa34887fe1a7738f452ed92c
Alexander Moore <alexandermoore202@gmail.com>
2025-08-06 21:15:34 -0400

    Added in sonner

--- src/app/ui/copy-button.tsx
+++ src/app/ui/copy-button.tsx
@@ -2,6 +2,7 @@
 
 import { CopyIcon, CheckmarkIcon } from "./icons";
 import { useState } from "react";
+import { toast, Toaster } from "sonner";
 
 export default function CopyButton() {
   const email = "teachercolleenm@gmail.com";
@@ -11,6 +12,8 @@ export default function CopyButton() {
     try {
       await navigator.clipboard.writeText(email);
       setCopied(true);
+      toast.success("Email copied to clipboard");
+      console.log("Email copied to clipboard:", email);
       setTimeout(() => setCopied(false), 2000); // reset after 2s
     } catch (err) {
       console.error("Failed to copy: ", err);
@@ -23,6 +26,7 @@ export default function CopyButton() {
       className="flex items-center p-2 hover:bg-white/5 rounded-lg transition cursor-pointer"
     >
       {copied ? <CheckmarkIcon /> : <CopyIcon />}
+      <Toaster position="bottom-center" richColors />
     </button>
   );
 }
